2e92aa41e12b5984a786c51a8c4fb56a4550c105
Kristian Honningsvåg <kristian@digitalunion.net>
2015-09-14 13:26:22 +0200

    sql scripts refactoring

--- sql_scripts/TrainingApplicationScript.sql
+++ sql_scripts/TrainingApplicationScript.sql
@@ -63,39 +63,4 @@ if object_id('exercise_set') is null
 	primary key(set_nr, exercise_id, workout_id)
 	);
 
-
-insert into trainer(email, pw, first_name, last_name)
-values ('ultimate.trainer@gmail.com', '1234', 'Jack', 'Strong'),
-('matt.forrest@gmail.com','PushUpsIsCool', 'Matt', 'Forrest'),
-('natalie.northwood@gmail.com','VeryLongPassword','Natalie','Northwood');
-
-insert into trainingprogram(trainer_id)
-values (1),(2),(3);
-
-insert into customer(email, pw, first_name, last_name, current_weight, height, date_Of_birth, sex, trainingprogram_id)
-values ('fancypants@gmail.com', 'DontAsk', 'Tiffany', 'McDonald', '55', '157', '1989-02-23', 'f', 1),
-('duke.davidson@gmail.com', 'DogsAreCool', 'Duke', 'Davidson', '68', '179', '1976-12-05', 'm', 1),
-('s.harrison@gmail.com', 'D45fsq23s54', 'Sam', 'Harrison', '77', '164', '1954-07-19', 'm', 1);
-
-insert into workout(trainingprogram_id, workout_name, workout_description)
-values(1, 'Legs of Endurance', 'Workout program with exercises that improves the endurence in your legs'),
-(1, 'Arms of Strength', 'Workout program with exercises that improves your arm strength'),
-(1, 'Abs of Steel', 'Workout program with exercises that gives you well defined abs');
-
-insert into exercise(exercise_name, exercise_description)
-values ('The 90-degree', 'Find a wall, support your back against it and make sure you have 90 degrees between the following: your legs and the floor, your legs and thighs. Hold for x number of seconds'),
-('Push-ups', 'Lay down and use your arms to push your body up, keep body stright. You are now in starting position. Now use your arms to lower your chest down towards the floor and then up agian, repeat x times.'),
-('Sit-ups', 'Lay on your back, hold arms crossed on your chest and use your abs to move your upper body up from the floor and down again.'),
-('Squats', 'Take a wide stance with your legs, then bend down with your ass outwards and move up again, repeat x times.'),
-('Pull-ups', 'Find something like horizontal bar you can hang from. Hang and pull your weight up so your head is above the bar, then lower yourself down and repeat x times'),
-('The plank','Lay down, then support your weight with your forearms so your body is straight and only your feet and forearms touch the ground, hold for x seconds');
-
-insert into exercise_set(set_nr, exercise_id, workout_id, repetitions_planed, repetitions_cleared, additional_load, additional_load_cleared, comment_by_user)
-values (1, 1, 1, 60, null, 0, null, null),
-(1, 4, 1, 15, null, 0, null, null),
-(1, 2, 2, 20, null, 0, null, null),
-(1, 5, 2, 5, null, 0, null, null),
-(1, 3, 3, 20, null, 0, null, null),
-(1, 6, 3, 60, null, 0, null, null);
-
 Use master;
--- sql_scripts/initial_setup.sql
+++ sql_scripts/initial_setup.sql
@@ -1,27 +1,26 @@
-
 /*
   If database already exists, disconnect all users, then disconnect from
-  the database by connecting to master instead. Finally drop database.
+  the database by connecting to master database. Finally drop database.
 */
 if DB_ID('training_application') IS NOT NULL
 BEGIN
 	ALTER DATABASE training_application SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 END
-
 USE master;
 DROP DATABASE training_application;
 GO
 
 CREATE DATABASE training_application;
 GO
+
 USE training_application;
 
 CREATE TABLE trainer (
 	trainer_id int NOT NULL PRIMARY KEY,
-	pw VARCHAR(40) NOT NULL,
-	first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR(30) NOT NULL,
-	email VARCHAR(40) NOT NULL,
+	pw VARCHAR(50) NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
 );
 
 CREATE TABLE training_program (
@@ -30,37 +29,39 @@ CREATE TABLE training_program (
 );
 
 CREATE TABLE customer (
-	customer_id int NOT NULL PRIMARY KEY,
+	customer_id INT NOT NULL PRIMARY KEY,
 	training_program_id int FOREIGN KEY REFERENCES training_program(training_program_id),
-	pw VARCHAR(40) NOT NULL,
-	first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR(30) NOT NULL,
-	email VARCHAR(40) NOT NULL,
-	weight int NOT NULL,
-	height int NOT NULL,
-	date_of_birth int NOT NULL,
-	sex BIT NOT NULL,
+	pw VARCHAR(50) NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	weight INT NOT NULL,
+	height INT NOT NULL,
+	date_of_birth DATE NOT NULL,
+	sex CHAR(1) NOT NULL CHECK (sex IN('f', 'm')),
 );
 
 CREATE TABLE workout (
 	workout_id INT PRIMARY KEY,
 	training_program_id INT NOT NULL FOREIGN KEY REFERENCES training_program(training_program_id),
 	workout_date DATE NOT NULL,
-	workout_name VARCHAR(30) NOT NULL,
-	workout_description VARCHAR(200) NOT NULL,
+	workout_name VARCHAR(50) NOT NULL,
+	workout_description TEXT NOT NULL,
 );
 
 CREATE TABLE exercise (
 	exercise_id INT NOT NULL PRIMARY KEY,
-	exercise_name VARCHAR(30) NOT NULL,
-	exercise_info VARCHAR(200) NOT NULL,
+	exercise_name VARCHAR(50) NOT NULL,
+	exercise_info TEXT NOT NULL,
 );
 
 CREATE TABLE exercise_set (
+	set_nr INT NOT NULL,
 	workout_id INT FOREIGN KEY REFERENCES workout(workout_id),
 	exercise_id INT FOREIGN KEY REFERENCES exercise(exercise_id),
-	repititions INT NOT NULL,
 	weight INT NOT NULL,
+	repititions INT NOT NULL,
 	repititions_cleared INT NOT NULL,
-	comment VARCHAR(200),
+	user_comment TEXT,
+	PRIMARY KEY(set_nr, exercise_id, workout_id)
 );
