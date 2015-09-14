b360e5ff237d4fa61fb6dd39bde715151e16b5a7
TMHolen <TorMx99@gmail.com>
2015-09-14 12:54:27 +0200

    Added stuff for the application

--- training_application_web/training_application.js
+++ training_application_web/training_application.js
@@ -1,5 +1,15 @@
-// JavaScript with jquery for animated effects and such
+// JavaScript with jquery for animation effects and such
 
 $(document).ready(function () {
-
+    $("#loginButton").click(function e() {
+        $("main").fadeOut("slow");
+    });
 });
+//Ajax to check US and PW
+
+//
+//$("button").click(function(){
+//    $("#div1").fadeOut();
+//    $("#div2").fadeOut("slow");
+//    $("#div3").fadeOut(3000);
+//});
