482d8f6fd226e4baa60e202a7397359ebdb2932d
Pitmairen <progrper@gmail.com>
2015-09-15 09:47:55 +0200

    Support static files for development server

--- training_application_php/public/index.php
+++ training_application_php/public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+// For serving static files for development only
+if (file_exists($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
+    return false;
+} 
 
 define('APP_ROOT', '../');
 
