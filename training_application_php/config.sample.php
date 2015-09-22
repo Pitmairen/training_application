7d9d2358e88a789847d09047d206eeaa771930ec
Pitmairen <progrper@gmail.com>
2015-09-22 09:41:36 +0200

    Add config sample file

---
+++ training_application_php/config.sample.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Define some constants and set up the data source
+ *
+ * Copy this file and rename it to config.php
+ */
+
+define('IS_DEBUGGING', true);
+define('COOKIE_SECRET', '<super-secret>');
+date_default_timezone_set('Europe/Oslo');
+
+
+// Create data source object here and store it in a 
+// variable called $ds.
+//
+// Example:
+$ds = new DataSourceSqlite('sqlite::memory:'); // Replace this with the real datasource
+
