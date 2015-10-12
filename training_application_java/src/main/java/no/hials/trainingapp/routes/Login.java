d270d3d5e6c21ae9449c499d98d19f644eb213fb
Pitmairen <progrper@gmail.com>
2015-10-12 20:38:53 +0200

    Refactor login route

--- training_application_java/src/main/java/no/hials/trainingapp/routes/Login.java
+++ training_application_java/src/main/java/no/hials/trainingapp/routes/Login.java
@@ -1,12 +1,10 @@
 package no.hials.trainingapp.routes;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import no.hials.trainingapp.auth.Auth;
 import no.hials.trainingapp.datasource.DataItem;
 import no.hials.trainingapp.datasource.DataSource;
-import no.hials.trainingapp.routing.TemplateRoute;
+import no.hials.trainingapp.routing.FormRoute;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -14,38 +12,39 @@ import static spark.Spark.halt;
 
 /**
  * Login a user
- * 
+ *
  * @author Per Myren <progrper@gmail.com>
  */
-public class Login extends TemplateRoute
+public class Login extends FormRoute
 {
 
-    private final List<String> mValidationErrors;
+    private final static String TEMPLATE_NAME = "login";
+
+    // Used to cache the user object so we don't have to reload it again
+    // after checking the username and password
     private DataItem mUserCache = null;
 
     public Login(DataSource datasource, Request req, Response resp)
     {
         super(datasource, req, resp);
-
-        mValidationErrors = new ArrayList<>();
     }
 
     @Override
     public ModelAndView get() throws Exception
     {
-        checkAuth();
+        checkUserNotAuthenticated();
 
-        return renderTemplate();
+        return renderTemplate(TEMPLATE_NAME);
     }
 
     @Override
     public ModelAndView post() throws Exception
     {
-        checkAuth();
-        
-        checkLogin();
+        checkUserNotAuthenticated();
+        checkUsernameAndPassword();
 
-        if (mValidationErrors.isEmpty()) {
+        if (!hasValidationErrors()) {
+
             Auth.loginUser(getRequest(),
                            mUserCache.getInteger("customer_id"),
                            mUserCache.getString("customer_first_name"));
@@ -54,18 +53,29 @@ public class Login extends TemplateRoute
             halt();
         }
 
-        return renderTemplate();
+        return renderTemplate(TEMPLATE_NAME);
     }
 
-    private void checkAuth(){
-        if(getCurrentUser().isAuthenticated()){
+    /**
+     * If user is already logged in there is no reason for him to log in so
+     * redirect to the index page
+     */
+    private void checkUserNotAuthenticated()
+    {
+        if (getCurrentUser().isAuthenticated()) {
             getResponse().redirect("/");
             halt();
         }
     }
-    
-    
-    private void checkLogin() throws SQLException
+
+    /**
+     * Check that the username and password is valid
+     *
+     * FIXME: Currently not checking password.
+     *
+     * @throws SQLException
+     */
+    private void checkUsernameAndPassword() throws SQLException
     {
 
         String username = getRequest().queryParams("username");
@@ -73,7 +83,7 @@ public class Login extends TemplateRoute
         DataItem customer = getDataSource().getCustomerByUsername(username);
 
         if (customer == null || !username.equals(customer.get("customer_email"))) {
-            mValidationErrors.add("Wrong username or password");
+            addValidationError("Wrong username or password");
         }
         else {
             mUserCache = customer;
@@ -81,12 +91,4 @@ public class Login extends TemplateRoute
 
     }
 
-    private ModelAndView renderTemplate()
-    {
-
-        setData("validationErrors", mValidationErrors);
-        setData("formData", getRequest().params());
-
-        return renderTemplate("login");
-    }
 }
