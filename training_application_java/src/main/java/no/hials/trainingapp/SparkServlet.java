fc16524bc5ad462a549ec2abb4a51f071e742fe4
Pitmairen <progrper@gmail.com>
2015-10-26 14:00:42 +0100

    Add optional auto reload and persistent sessions

---
+++ training_application_java/src/main/java/no/hials/trainingapp/SparkServlet.java
@@ -0,0 +1,21 @@
+package no.hials.trainingapp;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import spark.servlet.SparkApplication;
+
+/**
+ * Adds support for auto reload and persistent sessions
+ */
+public class SparkServlet implements SparkApplication {
+
+    @Override
+    public void init() {
+        try {
+            Main.main(null);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SparkServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
