1a95560b2931198200c5b584c6fcf2d99fd63d9c
TMHolen <TorMx99@gmail.com>
2015-10-26 11:08:38 +0100

    Removal of unused code

--- training_application_java/src/main/java/no/hials/trainingapp/datasource/DataSourceMssql.java
+++ training_application_java/src/main/java/no/hials/trainingapp/datasource/DataSourceMssql.java
@@ -1,7 +1,7 @@
 package no.hials.trainingapp.datasource;
 
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
-import com.microsoft.sqlserver.jdbc.SQLServerException;
+//import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+//import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -11,43 +11,27 @@ import java.util.List;
  */
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
-import java.util.ArrayList;
 
-public class DataSourceMssql implements DataSource {
 
-    private HikariDataSource sPool;
-    private Connection con;
+public class DataSourceMssql extends BaseDataSource {
 
-    // Used when a data source is used in a transaction
-    private final MssqlTransaction mTransaction;
-
-    private final String url = "jdbc:sqlserver://";
-    private final String serverName = "tmh-touchpc\\tmserver";
-    private final int portNumber = 1433;
-    private final String databaseName = "training_application";
-    private final String selectMethod = "cursor";
-    private final Boolean integratedSecurity = true;
-
-    public String getConnectionUrl() {
-        return url + this.serverName + ":" + this.portNumber + ";databaseName=" + this.databaseName + ";integratedSecurity=" + this.integratedSecurity + ";selectMethod=" + this.selectMethod + ";";
+    private String conURL = "jdbc:sqlserver://tmh-touchpc\\tmserver:1433;databaseName=training_application;integratedSecurity=true;selectMethod=true";
+    
+    private String getConnectionUrl() {
+        return conURL;
     }
 
-    public DataSourceMssql() throws ClassNotFoundException {
-        sPool = createConnectionPool(getConnectionUrl());
-
-        mTransaction = null;
-
+    public DataSourceMssql() {
+        super(null);
+//sPool = createConnectionPool(getConnectionUrl());
     }
 
-    private DataSourceMssql(MssqlTransaction trans) {
-        mTransaction = trans;
+    private DataSourceMssql(BaseTransaction trans) {
+        super(trans);
+    }
+
+    public static void initPool(String connectionString) throws ClassNotFoundException {
+        setConnectionPool(createConnectionPool(connectionString));
     }
 
     public List<DataItem> getCustomers() throws SQLException {
@@ -91,102 +75,6 @@ public class DataSourceMssql implements DataSource {
                 customerId, true);
     }
 
-    /**
-     * Returns a list of data items for the giver query.
-     *
-     * This method will make sure to use the correct connection if the data
-     * source is in a transaction.
-     *
-     * If not in a transaction the connection will automatically be returned to
-     * the pool after this method returns.
-     */
-    private List<DataItem> queryList(String query, Object... params) throws SQLException {
-
-        // If we are in a transaction use the transaction connection.
-        if (mTransaction != null) {
-            return queryList(mTransaction.getConnection(), query, params);
-        }
-
-        try (Connection con = sPool.getConnection()) {
-            return queryList(con, query, params);
-        }
-    }
-
-    /**
-     * Returns a single data item for the given query. This method should be
-     * used when the query always returns a single row.
-     *
-     * This method will make sure to use the correct connection if the data
-     * source is in a transaction.
-     *
-     * If not in a transaction the connection will automatically be returned to
-     * the pool after this method returns.
-     */
-    private DataItem querySingle(String query, Object... params) throws SQLException {
-
-        // If we are in a transaction use the transaction connection.
-        if (mTransaction != null) {
-            return querySingle(mTransaction.getConnection(), query, params);
-        }
-
-        try (Connection con = sPool.getConnection()) {
-            return querySingle(con, query, params);
-        }
-    }
-
-    private List<DataItem> createResultMap(ResultSet rs) throws SQLException {
-
-        ArrayList<DataItem> res = new ArrayList<>();
-
-        while (rs.next()) {
-
-            ResultSetMetaData meta = rs.getMetaData();
-            DataItem it = new DataItem();
-            int columnCount = meta.getColumnCount();
-            for (int i = 0; i < columnCount; i++) {
-                it.put(meta.getColumnLabel(i + 1), rs.getObject(i + 1));
-            }
-            res.add(it);
-        }
-
-        return res;
-    }
-
-//    private ResultSet executeQuery(Connection con, String query) throws SQLException {
-//        Statement statement = con.createStatement();
-//        
-//        return statement.executeQuery(query);
-//    }
-    private ResultSet executeQuery(Connection con, String query, Object... params)
-            throws SQLException {
-        PreparedStatement statement = con.prepareStatement(query);
-
-        bindParams(statement, params);
-
-        return statement.executeQuery();
-    }
-
-    private void bindParams(PreparedStatement statement, Object... params) throws SQLException {
-        for (int i = 0; i < params.length; i++) {
-            statement.setObject(i + 1, params[i]);
-        }
-
-    }
-
-    private HikariDataSource createConnectionPool(String connectionString) throws ClassNotFoundException {
-
-        Class.forName("org.sqlite.JDBC");
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(connectionString);
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-
-        HikariDataSource ds = new HikariDataSource(config);
-        return ds;
-
-    }
-
     @Override
     public void storeNewCustomer(DataItem data) throws SQLException {
 
@@ -209,80 +97,23 @@ public class DataSourceMssql implements DataSource {
 
     }
 
-    /**
-     * Begins a new database transaction. If something unexpected happens when
-     * creating the connection it will make sure the connection is returned to
-     * the pool.
-     */
-    private MssqlTransaction beginTransaction() throws SQLException {
-        Connection con = sPool.getConnection();
-        try {
-            return new MssqlTransaction(con);
-        } catch (Exception e) {
-            // If something happens make sure the connection is closed.
-            con.close();
-            throw e;
-        }
+    private static HikariDataSource createConnectionPool(String connectionString) throws ClassNotFoundException {
+
+        Class.forName("org.sqlite.JDBC");
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(connectionString);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource ds = new HikariDataSource(config);
+        return ds;
+
     }
 
     @Override
-    public void runTransaction(TransactionRunner runner) throws SQLException {
-        if (mTransaction != null) {
-            throw new RuntimeException("Nested transactions not suppored.");
-        }
-
-        try (MssqlTransaction tx = beginTransaction()) {
-            runner.runTransaction(tx, new DataSourceMssql(tx));
-        }
-    }
-
-    /**
-     * Mssql transaction
-     */
-    private static class MssqlTransaction
-            implements Transaction, AutoCloseable {
-
-        private final Connection mCon;
-
-        public MssqlTransaction(Connection con) throws SQLException {
-            mCon = con;
-            mCon.setAutoCommit(false);
-        }
-
-        @Override
-        public void commit() throws SQLException {
-            mCon.commit();
-        }
-
-        @Override
-        public void rollback() throws SQLException {
-            mCon.rollback();
-        }
-
-        /**
-         * Close the transaction and return the connection back to the
-         * connection pool
-         *
-         * @throws java.sql.SQLException
-         */
-        @Override
-        public void close() throws SQLException {
-            try {
-                mCon.rollback();
-                mCon.setAutoCommit(true);
-            } finally {
-                mCon.close();
-            }
-        }
-
-        /**
-         * Returns the connection associated with the transaction
-         *
-         * @return a db connection
-         */
-        private Connection getConnection() {
-            return mCon;
-        }
+    protected DataSource createTransactionDataSource(BaseTransaction tr) {
+        return new DataSourceMssql(tr);
     }
 
 }
