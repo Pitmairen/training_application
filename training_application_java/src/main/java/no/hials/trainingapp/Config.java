068d17716a56d85e00b27ba6f10afc7d9cdf2b5c
Pitmairen <progrper@gmail.com>
2015-11-12 11:15:55 +0100

    Comments

--- training_application_java/src/main/java/no/hials/trainingapp/Config.java
+++ training_application_java/src/main/java/no/hials/trainingapp/Config.java
@@ -11,56 +11,82 @@ import java.io.InputStream;
 import java.util.Properties;
 
 /**
- *
- * @author pitmairen
+ * Application configuration
  */
 public class Config {
-   
-    public static void loadConfigFromFile(String configFile) throws IOException{
+
+    /**
+     * Loads the config values from the specified config file
+     *
+     * @param configFile
+     * @throws IOException
+     */
+    public static void loadConfigFromFile(String configFile) throws IOException {
         sInstance = new Config();
         sInstance.readConfigFromFilename(configFile);
     }
-    
-    public static void loadConfigFromResource(String resourceName) throws IOException{
+
+    /**
+     * Loads the config values from the specified resource
+     *
+     * @param resourceName
+     * @throws IOException
+     */
+    public static void loadConfigFromResource(String resourceName) throws IOException {
         sInstance = new Config();
         sInstance.readConfigFromResource(resourceName);
     }
-    
-    
-    public static String getValue(String key){
+
+    /**
+     * Returns the config value for the key
+     *
+     * @param key
+     * @return
+     */
+    public static String getValue(String key) {
         return getInstance().props.getProperty(key);
     }
-    
-    public static String getValue(String key, String defaultValue){
+
+    /**
+     * Returns the config value for the key.
+     * 
+     * If it does not exist the defaultValue will be returned.
+     * 
+     * @param key
+     * @param defaultValue the value to return if the key does not exist
+     * @return 
+     */
+    public static String getValue(String key, String defaultValue) {
         return getInstance().props.getProperty(key, defaultValue);
     }
+
+    
     
     
     private static Config sInstance = null;
     private final Properties props;
-    
-    private static Config getInstance(){
-        if(sInstance == null){
+
+    private static Config getInstance() {
+        if (sInstance == null) {
             throw new RuntimeException("The config file must be loaded before "
                     + "reading from the config object.");
         }
         return sInstance;
     }
-    
-    
+
     private Config() throws IOException {
         props = new Properties();
     }
-    
-    private void readConfigFromFilename(String fileName) throws IOException{
-	try(InputStream input = new FileInputStream(fileName)){
-             props.load(input);
+
+    private void readConfigFromFilename(String fileName) throws IOException {
+        try (InputStream input = new FileInputStream(fileName)) {
+            props.load(input);
         }
     }
-    
-    private void readConfigFromResource(String resourceName) throws IOException{
-	try(InputStream input = getClass().getResourceAsStream(resourceName)){
-             props.load(input);
+
+    private void readConfigFromResource(String resourceName) throws IOException {
+        try (InputStream input = getClass().getResourceAsStream(resourceName)) {
+            props.load(input);
         }
     }
 }
