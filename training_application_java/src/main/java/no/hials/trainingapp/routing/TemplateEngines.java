ccd50de47c34b3d7e583e8f8d5c2dd69f08949c9
Pitmairen <progrper@gmail.com>
2015-11-17 10:27:09 +0100

    Templat loader fix for windows

--- training_application_java/src/main/java/no/hials/trainingapp/routing/TemplateEngines.java
+++ training_application_java/src/main/java/no/hials/trainingapp/routing/TemplateEngines.java
@@ -6,9 +6,15 @@ import com.mitchellbosecke.pebble.loader.ClasspathLoader;
 import com.mitchellbosecke.pebble.loader.DelegatingLoader;
 import com.mitchellbosecke.pebble.loader.FileLoader;
 import com.mitchellbosecke.pebble.loader.Loader;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import spark.TemplateEngine;
 import spark.template.pebble.PebbleTemplateEngine;
 
@@ -43,8 +49,8 @@ public class TemplateEngines {
     private static Loader createTemplateLoader() {
 
         List<Loader> loadingStrategies = new ArrayList<>();
-        loadingStrategies.add(new FileLoader());
         loadingStrategies.add(new TemplateLoader());
+        loadingStrategies.add(new FileLoader());
         DelegatingLoader loader = new DelegatingLoader(loadingStrategies);
 
         loader.setPrefix("templates");
@@ -57,9 +63,24 @@ public class TemplateEngines {
 
         @Override
         public Reader getReader(String templateName) throws LoaderException {
-            System.out.print("##### LOADING TEMPLATE #####: ");
-            System.out.println(templateName);
-            return super.getReader(templateName);
+
+            // Quick fix to make it work on windows. Don't know why the 
+            // original pebble classpath loader doesn't work.
+            ClassLoader classLoader = TemplateEngines.class.getClassLoader();
+            InputStream stream = classLoader.getResourceAsStream("templates/" + templateName + ".html");
+
+            if (stream == null) {
+                throw new LoaderException(null, "Could not find template \"" + templateName + "\"");
+            }
+
+            try {
+                return new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(TemplateEngines.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            throw new LoaderException(null, "Failed to render template \"" + templateName + "\"");
+
         }
 
     }
